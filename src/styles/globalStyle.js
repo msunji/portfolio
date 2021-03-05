63c43facd56dd2fd5fe90dd56596ed9164fad7f2
Marge <marge.consunji@gmail.com>
2021-03-05 15:18:01 +0800

    Removed font-family import from file. Was causing some issues

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -4,12 +4,25 @@ const GlobalStyle = createGlobalStyle`
     // Main colours
     :root {
         --main-bg-color: #F4EFE1;
+        --main-text-color: #2B230D;
     }
 
     body {
         background: var(--main-bg-color);
+        color: var(--main-text-color);
+        font-family: "Work Sans", sans-serif;
+        font-weight: 300;
+        font-size: 16px;
     }
 
+    h1, h2, h3, h4 {
+        font-family: "Sporting Grotesque", serif;
+    }
+    
+    p {
+        font-size: 2rem;
+        line-height: 1.2;
+    }
 `;
 
 export default GlobalStyle;
