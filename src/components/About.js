5f6b014e04b51f793ff06602d2217848ee499528
Marge <marge.consunji@gmail.com>
2021-03-05 20:38:31 +0800

    More edits to About page text

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,18 @@
 function About() {
   return (
     <div className="about-page">
-      <h1>Marge Consunji</h1>
+      <h1>Margaret "Marge" Consunji</h1>
+      <p>
+        I come from an economic research background. Numbers and making graphs
+        used to be my jam, but I've always had a knack for making things —
+        physical, digital. Baking, pottery, and graphic design, I've done it
+        all.
+      </p>
+      <p>
+        Previously, I was working with <b>Python</b> for data visualisation and
+        cleaning datasets. These days, I've been working on brushing up on my
+        React and Node.js skills.
+      </p>
     </div>
   );
 }
