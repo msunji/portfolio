0df3ce7041d5f845d23f8b4c9ae7d1c2b7a72a83
Marge <marge.consunji@gmail.com>
2021-03-06 16:20:15 +0800

    Edited blurb

--- src/components/About.js
+++ src/components/About.js
@@ -4,15 +4,22 @@ function About() {
       <h1>Margaret "Marge" Consunji</h1>
       <p>
         I come from an economic research background. Numbers and making graphs
-        used to be my jam, but I've always had a knack for making things —
-        physical, digital. Baking, pottery, and graphic design, I've done it
-        all.
+        used to be my jam, but I've always had a knack for making things both
+        physical and digital. Baking, arts and crafts, pottery, and graphic
+        design, I've done it all.
       </p>
       <p>
         Previously, I was working with <b>Python</b> for data visualisation and
-        cleaning datasets. These days, I've been working on brushing up on my
-        React and Node.js skills.
+        cleaning datasets. These days, I've been working on brushing up on my{" "}
+        <b>React</b> and <b>Node.js</b> skills.
       </p>
+
+      <p>
+        When I'm not working on web dev-related things, I'm either running,
+        baking bread, playing videogames, or learning foreign languages.
+      </p>
+
+      <p>View my CV here</p>
     </div>
   );
 }
