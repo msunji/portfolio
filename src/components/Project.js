8b3e60afee32d799f426f339efb083f4dae7ab76
Marge <marge.consunji@gmail.com>
2021-03-08 17:19:50 +0800

    Started working on project tile styling

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,10 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+
+const ProjectTile = styled.div`
+  position: relative;
+`;
+
+const ProjectInfo = styled.div`
+  background: white;
+  max-width: 600px;
+  padding: 3rem;
+  position: absolute;
+  z-index: 1000;
+  left: 30%;
+  top: 30%;
+`;
+
+const ProjectTitle = styled.h2``;
+
 function Project(props) {
   return (
-    <div className="project">
-      <p>{props.title}</p>
-      <p>{props.desc}</p>
+    <ProjectTile>
       <img src={props.thumbnail} alt={`Preview for ${props.title} project`} />
-    </div>
+      <ProjectInfo>
+        <ProjectTitle>{props.title}</ProjectTitle>
+        <p>{props.desc}</p>
+      </ProjectInfo>
+    </ProjectTile>
   );
 }
 
