f760341304019c6fa29b3842e657f6de17e91cc9
Marge <marge.consunji@gmail.com>
2021-03-10 13:58:45 +0800

    Content edits

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,20 @@
+import styled from "styled-components";
+
+const AboutContainer = styled.div`
+  margin-bottom: 10em;
+
+  & > p:last-of-type {
+    margin-bottom: 5em;
+  }
+`;
+
 function About() {
   return (
-    <div className="about-page">
-      <h1>Margaret "Marge" Consunji</h1>
+    <AboutContainer>
       <p>
-        I come from an economic research background. Numbers and making graphs
-        used to be my jam, but I've always had a knack for making things both
-        physical and digital. Baking, arts and crafts, pottery, and graphic
-        design, I've done it all.
+        My friends call me Marge. I come from an economic research background,
+        but I've always had a knack for making things both physical and digital.
+        Baking, arts and crafts, pottery, and graphic design, I've done it all.
       </p>
       <p>
         Previously, I was working with <b>Python</b> for data visualisation and
@@ -19,8 +27,10 @@ function About() {
         baking bread, playing videogames, or learning foreign languages.
       </p>
 
-      <p>View my CV here</p>
-    </div>
+      <a className="boxLink" href="/">
+        View my CV
+      </a>
+    </AboutContainer>
   );
 }
 
