b4cc35921b49caf313389d7e4cc7dacfeeb206fe
Marge <marge.consunji@gmail.com>
2021-05-25 12:04:28 +0800

    Added placeholder text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <section className="max-w-2xl mx-auto">
+        <h1>Hey there, I'm Marge Consunji</h1>
+        <p>I have a background in economic research,  but I've been doing more freelance front-end dev work as of late. There's not much to see here yet, so come back later.</p>
+        <p>In the meantime, here's my <a href="https://github.com/msunji">Github account</a>. Always open to doing more front-end work as well as English-Filipino/Tagalog translations.</p>
+      </section>
     </div>
   );
 }
