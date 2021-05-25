5b5cf6d9fc27097a64aa1603b0b1442234aa71a1
Marge <marge.consunji@gmail.com>
2021-05-25 13:16:34 +0800

    Minor edits to copy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
         <p className="uppercase tracking-wide mb-1 font-mono">Hey there, I'm</p>
         <h1 className="text-4xl mb-2 font-extrabold">Marge Consunji <span>✌️</span></h1>
         <p className="mb-4 leading-7">I have a background in economic research,  but over the last year or so, I've been transitioning into web development. Still working on getting this portfolio sorted out, so come back later.</p>
-        <p className="mb-4 leading-7">In the meantime, here's my <a href="https://github.com/msunji">Github account</a>. Always open to doing more front-end work, as well as English-Filipino/Tagalog translations.</p>
+        <p className="mb-4 leading-7">In the meantime, here's my <a href="https://github.com/msunji">Github account</a>. I'm always keen to do more front-end work, as well as English-Filipino/Tagalog translations.</p>
       </section>
     </div>
   );
