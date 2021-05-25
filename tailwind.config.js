646b6f17b1b88f291ddbddaf3e9fd7d4e7517750
Marge <marge.consunji@gmail.com>
2021-05-25 13:12:30 +0800

    Simple styling done

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,18 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      transparent: 'transparent',
+      yellow: colors.amber,
+      gray: colors.coolGray,
+    },
     fontFamily: {
       'sans': ['ui-sans-serif', 'system-ui', 'Helvetica Neue', 'sans-serif'],
-      'mono': ['ui-monospace', 'Monaco', 'monospace'],
+      'mono': ['ui-monospace', "Liberation Mono", 'monospace'],
     },
-    extend: {},
+    extend: {
+      textColor: ['visited'],
+    },
   },
   variants: {
     extend: {},
