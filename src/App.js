f6a79d77339358ae47582820b64d33e1b389c8f0
Marge <marge.consunji@gmail.com>
2021-05-25 13:12:23 +0800

    Sorted out copy

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 function App() {
   return (
     <div className="App">
-      <section className="max-w-2xl mx-auto">
-        <h1>Hey there, I'm Marge Consunji</h1>
-        <p>I have a background in economic research,  but I've been doing more freelance front-end dev work as of late. There's not much to see here yet, so come back later.</p>
-        <p>In the meantime, here's my <a href="https://github.com/msunji">Github account</a>. Always open to doing more front-end work as well as English-Filipino/Tagalog translations.</p>
+      <section className="max-w-xl mx-auto mt-32 px-8">
+        <p className="uppercase tracking-wide mb-1 font-mono">Hey there, I'm</p>
+        <h1 className="text-4xl mb-2 font-extrabold">Marge Consunji <span>✌️</span></h1>
+        <p className="mb-4 leading-7">I have a background in economic research,  but over the last year or so, I've been transitioning into web development. Still working on getting this portfolio sorted out, so come back later.</p>
+        <p className="mb-4 leading-7">In the meantime, here's my <a href="https://github.com/msunji">Github account</a>. Always open to doing more front-end work, as well as English-Filipino/Tagalog translations.</p>
       </section>
     </div>
   );
