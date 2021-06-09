169541df34dc8754cbe7cb0ec2bf88d6b352c11c
Marge <marge.consunji@gmail.com>
2021-06-09 16:34:02 +0800

    Installed react-icons

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaCircle } from "react-icons/fa";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 
@@ -12,6 +13,24 @@ const NavContainer = styled.div`
 
 const NavHome = styled(Link)`
   border-bottom: none;
+  position: relative;
+  z-index: 1;
+
+  &:before {
+    content: "\\3030";
+    position: absolute;
+    top: 20%;
+    z-index: -2;
+    transform: scale(4);
+    color: orange;
+  }
+
+  & p {
+    font-size: 0.9em;
+    line-height: 1;
+    margin-bottom: 0;
+    padding-top: 0.05rem;
+  }
 `;
 
 const NavLinks = styled.ul`
@@ -21,9 +40,14 @@ const NavLinks = styled.ul`
   li {
     display: flex;
     flex-direction: row;
+    align-items: center;
 
     &:not(:last-of-type) {
       margin-right: 3em;
+    }
+
+    & svg {
+      margin-right: 10px;
     }
   }
 
@@ -39,16 +63,21 @@ function Header() {
     <>
       <div className="container">
         <NavContainer>
-          <NavHome to="/">MCONSUNJI</NavHome>
+          <NavHome to="/">
+            <p>Marge</p>
+            <p>Consunji</p>
+          </NavHome>
 
           <NavLinks>
             {/* <li>
           <a href="">Projects</a>
         </li> */}
             <li>
+              <FaCircle />
               <a href="/about">About</a>
             </li>
             <li>
+              <FaCircle />
               <a href="https://notes.mconsunji.com">Notes</a>
             </li>
           </NavLinks>
