b77be91abe89d840719a6d9a3ccf9e44c241ffa7
Marge <marge.consunji@gmail.com>
2021-06-09 15:58:42 +0800

    Added grid

--- src/components/About.js
+++ src/components/About.js
@@ -1,35 +1,47 @@
 import styled from "styled-components";
 
 const AboutContainer = styled.div`
-  margin-bottom: 10em;
+  background: #ffba00;
+  padding-top: 6rem;
+  padding-bottom: 6rem;
+`;
 
-  & > p:last-of-type {
-    margin-bottom: 5em;
-  }
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr;
+  grid-gap: 6rem;
 `;
 
 function About() {
   return (
-    <AboutContainer>
-      <p>
-        My friends call me Marge. I come from an economic research background,
-        but I've always had a knack for making things both physical and digital.
-        Baking, arts and crafts, pottery, and graphic design, I've done it all.
-      </p>
-      <p>
-        Previously, I was working with <b>Python</b> for data visualisation and
-        cleaning datasets. These days, I've been working on brushing up on my{" "}
-        <b>React</b> and <b>Node.js</b> skills.
-      </p>
+    <AboutContainer id="about">
+      <Grid className="container">
+        <div>
+          <h3>About Me</h3>
+        </div>
+        <div>
+          <p>
+            My friends call me Marge. I come from an economic research
+            background, but I've always had a knack for making things both
+            physical and digital. Baking, arts and crafts, pottery, and graphic
+            design, I've done it all.
+          </p>
+          <p>
+            Previously, I was working with <b>Python</b> for data visualisation
+            and cleaning datasets. These days, I've been working on brushing up
+            on my <b>React</b> and <b>Node.js</b> skills.
+          </p>
 
-      <p>
-        When I'm not working on web dev-related things, I'm either running,
-        baking bread, playing videogames, or learning foreign languages.
-      </p>
+          <p>
+            When I'm not working on web dev-related things, I'm either running,
+            baking bread, playing videogames, or learning foreign languages.
+          </p>
 
-      <a className="boxLink" href="/">
-        View my CV
-      </a>
+          <a className="boxLink" href="/">
+            View my CV
+          </a>
+        </div>
+      </Grid>
     </AboutContainer>
   );
 }
