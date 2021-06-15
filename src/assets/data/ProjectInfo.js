ae868e4ded5302dfd3952095d384102ffb21b759
Marge <marge.consunji@gmail.com>
2021-06-15 15:25:25 +0800

    Data for project list

---
+++ src/assets/data/ProjectInfo.js
@@ -0,0 +1,14 @@
+export const ProjectInfo = [
+  {
+    id: 0,
+    title: "Personal Blog",
+    desc: "Lorem ipsum",
+    thumb: "http://placecorgi.com/450/500",
+  },
+  {
+    id: 1,
+    title: "IDAS Conference Website",
+    desc: "Lorem ipsum",
+    thumb: "http://placecorgi.com/450/500",
+  },
+];
