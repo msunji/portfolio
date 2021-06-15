d0db803f7d8e953fc7014fe8bbd75af5df1c461f
Marge <marge.consunji@gmail.com>
2021-06-15 16:24:21 +0800

    Added short project descriptions

--- src/assets/data/ProjectInfo.js
+++ src/assets/data/ProjectInfo.js
@@ -1,14 +1,16 @@
 export const ProjectInfo = [
   {
     id: 0,
-    title: "Personal Blog",
-    desc: "Lorem ipsum",
+    title: "Notes",
+    desc:
+      "A place for me to store and keep track of personal notes and project notes. Built with Next.js and Contentful.",
     thumb: "http://placecorgi.com/450/500",
   },
   {
     id: 1,
     title: "IDAS Conference Website",
-    desc: "Lorem ipsum",
+    desc:
+      "A remake of a conference website I made for the International Doctoral Programme in Asia-Pacific Studies at National Chengchi University. Built with Next.js",
     thumb: "http://placecorgi.com/450/500",
   },
 ];
