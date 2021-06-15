01e2dfb39d595db5f985395490d5e93249625060
Marge <marge.consunji@gmail.com>
2021-06-15 16:20:23 +0800

    Removed SVG blob, changed z-index

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
+import React from "react";
 import styled from "styled-components";
+import Button from "./Button";
 
 const AboutContainer = styled.section`
   background: #ffba00;
   position: relative;
-  z-index: -1;
+  // z-index: -1;
 `;
 
 const Grid = styled.div`
@@ -11,22 +13,12 @@ const Grid = styled.div`
   grid-template-columns: 1fr 2fr;
   grid-gap: 6rem;
   position: relative;
-  z-index: 1;
+  z-index: 10;
 `;
 
 function About() {
   return (
     <AboutContainer id="about">
-      <svg
-        class="blob--blue"
-        viewBox="0 0 200 200"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M35.5,-48.1C42.8,-43.5,43.5,-28.9,43.8,-16.8C44.2,-4.8,44.2,4.8,39.9,11.4C35.7,18.1,27.1,21.7,19.7,23.7C12.3,25.6,6.2,25.8,-3.1,30.1C-12.4,34.4,-24.8,42.8,-39.8,43.3C-54.8,43.8,-72.5,36.6,-82,23C-91.4,9.3,-92.7,-10.6,-86.6,-27.7C-80.5,-44.8,-66.9,-59.2,-51.2,-61C-35.6,-62.8,-17.8,-52.2,-1.9,-49.7C14.1,-47.1,28.1,-52.6,35.5,-48.1Z"
-          transform="translate(100 100)"
-        />
-      </svg>
       <Grid className="container">
         <div>
           <h3>
@@ -52,9 +44,7 @@ function About() {
             baking bread, playing videogames, or learning foreign languages.
           </p>
 
-          <a className="boxLink" href="/">
-            View my CV
-          </a>
+          <Button text="View my CV" link="" />
         </div>
       </Grid>
     </AboutContainer>
