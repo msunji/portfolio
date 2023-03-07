4ab21efb05309c0d845e830c509c9d0a09d170fa
Marge <marge.consunji@gmail.com>
2023-03-07 12:54:02 +0800

    Update Tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    // Example content paths...
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  darkMode: 'media', // or 'media' or 'class'
   theme: {
     colors: {
       transparent: 'transparent',
@@ -16,9 +20,6 @@ module.exports = {
     extend: {
       textColor: ['visited'],
     },
-  },
-  variants: {
-    extend: {},
   },
   plugins: [],
 }
