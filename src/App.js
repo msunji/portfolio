d576c451efdf791e9c5f0702e4a5c3b0b587a9b2
Marge <marge.consunji@gmail.com>
2023-09-07 14:47:17 +0800

    Updated projects

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ function App() {
         <p className="leading-7 text-4">Hey there, it's nice to meet you. I'm Marge Consunji. Over the last few years, I have been transitioning from economic research to design and frontend development work. I build things with React and Next.js, but am always open to exploring different technologies. When I'm not at my desk, you can find me baking, drawing, playing video games, or hanging out with my cats 🐱</p>
       </section>
       <section className="max-w-2xl mt-32 px-8 mb-4 space-y-4">
-        <h2 className="text-4xl font-extrabold">Currently</h2> 
-        <ul>
-          <li><a href="https://github.com/msunji/puppeteer-dividends">Dividend News</a> - A web scraper built with Puppeteer and Node that collates the latest dividend announcements from Philippine Stock Exchange Edge (PSE Edge)</li>
+        <h2 className="text-4xl font-extrabold">Projects</h2> 
+        <ul className="space-y-4">
+          <li><a href="https://github.com/msunji/dividend-news">Dividend News</a> - A web scraper built with Cheerio, Node.js and Firebase functions that routinely checks Philippine Stock Market (PSE) Edge for dividend announcements. Daily announcements are then compiled and emailed to a list of recipients through Sendgrid API.</li>
+          <li><a href="https://github.com/msunji/spring-publications-tw">Spring Publications TW</a> - A catalogue and ordering platform (doesn't take payments at the moment) for Spring Book's Taiwan distributor. Uses Airtable as an easy to use, lightweight database, as well as Next.js and Tailwind CSS.</li>
         </ul>
       </section>
     </div>
